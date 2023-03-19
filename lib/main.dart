bf1b3bab29c23424d1edcbb9747db3d3158c816f
ShagunMaurya17 <shagun.maurya17@gmail.com>
2023-03-19 18:47:16 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("welcome to 30days of flutter by shagun maurya"),
-        ),
-      ),
-    ));
+       home: homePage(),);
   }
 }
